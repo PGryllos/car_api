1014f404f6439f7b100952e867a4b2398a264825
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-09-11 20:12:16 +0300

    Create car model to represent cars in GeoData Base

--- Ally/cars_api/models.py
+++ Ally/cars_api/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+# The car model. Every row represents a car using its position
+class cars(models.Model):
+    # overriding default manager with GeoManager
+    objects = models.GeoManager()
+
+    lnglat = models.PointField(srid=4326)   # position of the car
+    desc = models.CharField(max_length=80)  # description of the area
