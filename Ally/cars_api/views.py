ea6d0f417aa6cca099838b5197ebf2763377fb67
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-09-12 03:10:22 +0300

    Add test JsonResponse

--- Ally/cars_api/views.py
+++ Ally/cars_api/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def tenNearestCars(request, location=None):
     if request.method =="POST":
         pass
 
     if request.method =="GET":
-        location = request.GET.get('location')
+        lat, lng = str(request.GET.get('location')).split(',')
 
-    return HttpResponse("Hello, world.")
+    data = {
+                "cars": [
+                    {
+                    "description": "test JsonResponse",
+                    "latitude": lat,
+                    "longitute": lng
+                    }
+                ]
+            }
+    return JsonResponse(data)
