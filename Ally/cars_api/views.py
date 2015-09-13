d921b37e5c26859887ac0f63108afe9404bffc72
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-09-13 17:33:14 +0300

    Add Rtree indexing query

--- Ally/cars_api/views.py
+++ Ally/cars_api/views.py
@@ -11,18 +11,35 @@ def tenNearestCars(request, location=None):
     if request.method == "GET":
         lng, lat = str(request.GET.get('location')).split(',')
 
-        # naive approach for knn
-        query1 = """ SELECT * FROM cars_api_car cars
-        ORDER BY ST_Distance(cars.lnglat, ST_PointFromText('POINT(%s %s)', 4326))
-        ASC LIMIT 10 """ %(lng, lat)
+        # naive approach for knn. Doesn't use Rtree indexing
+        query1 = """
+            SELECT
+                *
+            FROM
+                cars_api_car cars
+            ORDER BY
+                ST_Distance(cars.lnglat, ST_PointFromText('POINT(%s %s)', 4326))
+            ASC LIMIT 10""" %(lng, lat)
+
+        # another query that uses Rtree indexing
+        query2 = """
+            SELECT
+                *
+            FROM
+                cars_api_car cars
+            ORDER BY
+                cars.lnglat <->
+                'SRID=4326;POINT(%s %s)'::geometry
+            DESC LIMIT 10""" %(lng, lat)
 
         data = { "cars": [] }
 
-    for obj in car.objects.raw(query1):
-        data["cars"].append({
-            "description": obj.desc,
-            "latitude": obj.lnglat.x,
-            "longitute": obj.lnglat.y
-            })
+        for obj in car.objects.raw(query2):
+            data["cars"].append(
+                    {
+                        "description": obj.desc,
+                        "longitute": obj.lnglat.x,
+                        "latitude": obj.lnglat.y
+                    })
 
-    return JsonResponse(data)
+        return JsonResponse(data)
