348905ab823e6f70e7bb58916368042d151e030a
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-09-18 19:14:09 +0300

    Add boostrap
    also add favicon.ico

--- Ally/Ally/settings.py
+++ Ally/Ally/settings.py
@@ -104,4 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "cars_api", "static"),
+)
